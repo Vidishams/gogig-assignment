955d6461564cdd129ff884ce5f7579f5f4983094
Vidishams <mssrividisha@gmail.com>
2026-08-14 01:42:06 +0530

    fix worker module import

--- api/app/queue.py
+++ api/app/queue.py
@@ -5,4 +5,9 @@ from rq import Queue
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
 redis_conn = Redis.from_url(REDIS_URL)
-image_queue = Queue("image_analysis", connection=redis_conn, default_timeout=120)
+
+image_queue = Queue(
+    "image_analysis",
+    connection=redis_conn,
+    default_timeout=120
+)
--- worker/run.py
+++ worker/run.py
@@ -10,6 +10,12 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
 if __name__ == "__main__":
     conn = Redis.from_url(REDIS_URL)
+
     queue = Queue("image_analysis", connection=conn)
-    worker = Worker([queue], connection=conn)
+
+    worker = Worker(
+        [queue],
+        connection=conn
+    )
+
     worker.work(with_scheduler=True)
